1f29216eb935279ae03ef5b231344de619adf7b2
PaulArias27 <paularias2727@gmail.com>
2026-05-23 19:06:42 -0500

    PA: se creo el paquete servicio y la clase ProductoServicio con los respectivos servicios

---
+++ src/main/java/com/krakedev/productos/servicio/ProductoService.java
@@ -0,0 +1,81 @@
+package com.krakedev.productos.servicio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.krakedev.productos.entidad.Producto;
+
+@Service
+public class ProductoService {
+
+    private ArrayList<Producto> productos = new ArrayList<Producto>();
+
+    // Buscar por código
+    public Producto buscarPorCodigo(String codigo) {
+
+        for (Producto p : productos) {
+
+            if (p.getCodigo().equals(codigo)) {
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    // Crear producto
+    public Producto crear(Producto producto) {
+
+        Producto existente = buscarPorCodigo(producto.getCodigo());
+
+        if (existente != null) {
+            return null;
+        }
+
+        productos.add(producto);
+
+        return producto;
+    }
+
+    // Listar productos
+    public List<Producto> listar() {
+        return productos;
+    }
+
+    // Actualizar producto
+    public Producto actualizar(String codigo,
+            Producto productoActualizado) {
+
+        Producto producto = buscarPorCodigo(codigo);
+
+        if (producto != null) {
+
+            producto.setNombre(
+                    productoActualizado.getNombre());
+
+            producto.setPrecio(
+                    productoActualizado.getPrecio());
+
+            return producto;
+        }
+
+        return null;
+    }
+
+    // Eliminar producto
+    public boolean eliminar(String codigo) {
+
+        Producto producto = buscarPorCodigo(codigo);
+
+        if (producto != null) {
+
+            productos.remove(producto);
+
+            return true;
+        }
+
+        return false;
+    }
+}
